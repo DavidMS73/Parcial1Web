21c089fc53b4aaad81cde29e153597e492249740
German <gd.martinez@uniandes.edu.co>
2020-09-23 12:32:49 -0500

    Parcial1

--- script.js
+++ script.js
@@ -1,17 +1,98 @@
 const dataDir =
   "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 
-const cart = [];
+let carP = {};
+let allProductos = [];
+let orderDef = [];
+
+function totalItem() {
+  let total = 0;
+  for (const i in carP) {
+    total += carP[i];
+  }
+  let text = " item";
+  if (total > 1) {
+    text += "s";
+  }
+  document.getElementById("numeroProductos").innerText = total + text;
+}
+
+function fillTableOrder() {
+  let tabla = document.getElementById("orderDetailBody");
+  while (tabla.firstChild) {
+    tabla.removeChild(tabla.firstChild);
+  }
+  let totalSum = 0;
+  orderDef.forEach((element, index) => {
+    let row = tabla.insertRow(-1);
+
+    let item = document.createElement("th");
+    let node1 = document.createTextNode(index + 1);
+    item.appendChild(node1);
+    row.appendChild(item);
+
+    let qty = row.insertCell(1);
+    qty.innerHTML = element.quantity;
+
+    let description = row.insertCell(2);
+    description.innerHTML = element.description;
+
+    let unitPrice = row.insertCell(3);
+    unitPrice.innerHTML = element.unitPrice;
+
+    let amount = row.insertCell(4);
+    let partialSum = element.unitPrice * element.quantity;
+    totalSum += partialSum;
+    amount.innerHTML = partialSum;
+  });
+
+  document.getElementById("totalSum").innerHTML = totalSum;
+}
+
+function addProductInDetail() {
+  for (const idName in carP) {
+    qtyProduct = carP[idName];
+    data = allProductos.find((element) => element.name === idName);
+
+    let order = {
+      item: orderDef.length + 1,
+      quantity: qtyProduct,
+      description: idName,
+      unitPrice: data.price,
+    };
+
+    data2 = orderDef.find((element) => element.description === idName);
+    if (data2 != undefined) {
+      data2.quantity = qtyProduct;
+    } else {
+      orderDef.push(order);
+    }
+  }
+
+  fillTableOrder();
+}
+
+function add2Car(item2Add) {
+  let idItem = item2Add.id;
+  if (carP[idItem] === undefined) {
+    carP[idItem] = 0;
+  }
+  carP[idItem] += 1;
+  totalItem();
+  addProductInDetail();
+}
 
 function addProductInACategory(productsList, categoryName) {
   categoryName = categoryName.replaceAll(" ", "");
   let idHtml = document.getElementById(categoryName + "Cards");
   productsList.forEach((element, index) => {
+    allProductos.push(element);
+
     let colSpace = document.createElement("div");
     colSpace.className = "col mb-4";
 
     let rowData = document.createElement("div");
-    rowData.className = "card";
+    rowData.className = "card h-100";
 
     let img = document.createElement("img");
     img.src = `${element.image}`;
@@ -41,6 +122,9 @@ function addProductInACategory(productsList, categoryName) {
     p3.className = "btn btn-dark";
     let node4 = document.createTextNode("Add to car");
     p3.appendChild(node4);
+    p3.onclick = function () {
+      add2Car(this);
+    };
 
     body.appendChild(title);
     body.appendChild(p1);
@@ -72,7 +156,8 @@ function borrarTodo() {
     tableOrder.removeChild(tableOrder.firstChild);
   }
   boton.setAttribute("data-dismiss", "modal");
-  cart = [];
+  document.getElementById("numeroProductos").innerText = 0 + " items";
+  carP = {};
 }
 
 function showBurgers() {
@@ -112,6 +197,10 @@ function hideAllSections() {
   }
 }
 
+function confirmation() {
+  console.log(orderDef);
+}
+
 let burguersBtn = document.getElementById("burguersBtn");
 burguersBtn.addEventListener("click", showBurgers);
 
@@ -130,5 +219,8 @@ DandSBtn.addEventListener("click", showDandS);
 let orderDetailBtn = document.getElementById("orderDetailBtn");
 orderDetailBtn.addEventListener("click", showOrderDetail);
 
-let boton = document.getElementById("deleteAll");
-boton.addEventListener("click", borrarTodo);
+let deleteAllBtn = document.getElementById("deleteAll");
+deleteAllBtn.addEventListener("click", borrarTodo);
+
+let placeOrderBtn = document.getElementById("placeOrderBtn");
+placeOrderBtn.addEventListener("click", confirmation);
